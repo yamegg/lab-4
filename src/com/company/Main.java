752aaecfab41ce15eadb6980b9c830ec3ee13927
yamegg <denchikfd@gmail.com>
2021-11-23 14:15:26 +0300

    switch to branch branch2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 System.out.println("Hello World!");
 System.out.println("second commit");
+System.out.println("Next commit");
+System.out.println("branch2");
     }
 }
